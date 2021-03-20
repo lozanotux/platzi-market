e0131bf090a194ce5de8ae7fb4b3c664024096e0
Juan Lozano <juan.lozano@vault-it.com.ar>
2021-03-20 00:01:29 -0300

    First commit

---
+++ src/test/java/com/redhat/market/PlatziMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
